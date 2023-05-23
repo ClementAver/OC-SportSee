d5df874ed7b5591427d3138af3c4ae1ee9f4c67d
ClementAver <clement.aver@yahoo.fr>
2023-05-23 13:55:22 +0200

    Adds store.js

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Store from "../../store/Store";
 
 export default function Dashboard() {
   const { id } = useParams();
+  const [user, setUser] = useState(null);
 
-  return (
-    <>
-      <h1>Dashboard</h1>
-      <p>{id}</p>
-    </>
-  );
+  useEffect(() => {
+    const mocked = false;
+    const store = new Store(parseInt(id), mocked);
+
+    if (mocked) {
+      setUser(store.getUserMainData());
+    } else {
+      store.getUserMainData().then((res) => {
+        setUser(res.data);
+      });
+    }
+  }, [id]);
+
+  if (user !== null) {
+    return (
+      <>
+        <h1>Dashboard</h1>
+        <p>{user.userInfos.firstName}</p>
+      </>
+    );
+  }
 }
